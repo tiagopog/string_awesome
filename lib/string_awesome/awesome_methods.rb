1447da80921577f003c8e62895341b906e0706bb
Tiago Guedes <tiagopog@gmail.com>
2013-12-07 00:23:30 -0200

    Testing the new indentation style.

--- lib/string_awesome/awesome_methods.rb
+++ lib/string_awesome/awesome_methods.rb
@@ -6,37 +6,37 @@ require 'sanitize'
 module StringAwesome
   # These methods are all included into the String class.
   module AwesomeMethods
-  	# Replaces \n to <br /> tags.
-  	# 
-  	# Example:
-  	#   >> "Hello 
-  	#          world!".nl2br 
-  	#   => "Hello <br/ > world!"
+    # Replaces \n to <br /> tags.
+    # 
+    # Example:
+    #   >> "Hello 
+    #          world!".nl2br 
+    #   => "Hello <br/ > world!"
 
-	  def nl2br
-	  	self.gsub /\n/, '<br />'
-	  end
+    def nl2br
+      self.gsub /\n/, '<br />'
+    end
 
-	  # Converts the string to the title style and prevents other 
-	  # letters in the middle of the word from being uppercase.
-		# 
-		# Example:
-		#   >> 'loREm IPsuM DOLOR'.to_title
-		#   => 'Lorem Ipsum Dolor'
-	
-		def to_title
-			self.downcase.titleize
-		end
+    # Converts the string to the title style and prevents other 
+    # letters in the middle of the word from being uppercase.
+    # 
+    # Example:
+    #   >> 'loREm IPsuM DOLOR'.to_title
+    #   => 'Lorem Ipsum Dolor'
 
-		# Removes HTML tags from text.
-		# 
-		# Example:
-		#   >> '<h1><a href="http://somecoolurl.com">Aloha!</a></h1>'.strip_tags 
-		#   => 'Aloha!'
-		#
-		# Arguments:
-		#  allow_whitespace: (Boolean)
-		#    - Let it returns the replaced block HTML tags as whitespaces.
+    def to_title
+      self.downcase.titleize
+    end
+
+    # Removes HTML tags from text.
+    # 
+    # Example:
+    #   >> '<h1><a href="http://somecoolurl.com">Aloha!</a></h1>'.strip_tags 
+    #   => 'Aloha!'
+    #
+    # Arguments:
+    #  allow_whitespace: (Boolean)
+    #    - Let it returns the replaced block HTML tags as whitespaces.
     
     def strip_tags(allow_whitespace = false)
     	str = Sanitize.clean self
--- spec/awesome_methods_spec.rb
+++ spec/awesome_methods_spec.rb
@@ -7,11 +7,11 @@ require 'spec_helper'
 # 
 describe 'String#nl2br' do
   it 'should replace "\n" to "<br />"' do
-  	"break\nrow".nl2br.should eq 'break<br />row'
+    "break\nrow".nl2br.should eq 'break<br />row'
   end
 
-	it 'should not replace "\n" to "<br />" when "\n" is literal' do
-  	"\\no break row".nl2br.should_not eq '<br />o break row'
+  it 'should not replace "\n" to "<br />" when "\n" is literal' do
+    "\\no break row".nl2br.should_not eq '<br />o break row'
   end  
 end
 
@@ -20,7 +20,7 @@ end
 # 
 describe 'String#to_title' do
   it 'should format the text properly to looks like a title' do
-  	"here's aN AWEsome TiTle!".to_title.should eq "Here's An Awesome Title!"
+    "here's aN AWEsome TiTle!".to_title.should eq "Here's An Awesome Title!"
   end
 end
 
@@ -29,15 +29,15 @@ end
 # 
 describe 'String#strip_tags' do
   it 'should remove any HTML tag from a given text' do
-  	'<h1><a href="http://somecoolurl.com">Aloha!</a></h1>'.strip_tags.should eq 'Aloha!'
+    '<h1><a href="http://somecoolurl.com">Aloha!</a></h1>'.strip_tags.should eq 'Aloha!'
   end
 
   it 'should remove by default the whitespaces that were allocated in place of HTML tags' do
-  	'<h1>no whitespaces</h1>'.strip_tags.should eq 'no whitespaces'
+    '<h1>no whitespaces</h1>'.strip_tags.should eq 'no whitespaces'
   end
 
   it 'should not remove the whitespaces that were allocated in place of HTML tags, when required' do
-  	'<h1>whitespaces</h1>'.strip_tags(true).should eq ' whitespaces '
+    '<h1>whitespaces</h1>'.strip_tags(true).should eq ' whitespaces '
   end
 end
 
@@ -46,11 +46,11 @@ end
 # 
 describe 'String#slug' do
   it 'should parse the text to an URL valid format, downcase by default' do
-  	'Lorem IPSUM Dolor?'.slug.should eq 'lorem-ipsum-dolor'
+    'Lorem IPSUM Dolor?'.slug.should eq 'lorem-ipsum-dolor'
   end
 
   it 'should parse the text to an URL valid format without forcing it to downcase' do
-  	'Lorem Ipsum Dolor 2013!'.slug(false).should eq 'Lorem-Ipsum-Dolor-2013'
+    'Lorem Ipsum Dolor 2013!'.slug(false).should eq 'Lorem-Ipsum-Dolor-2013'
   end
 end
 
@@ -59,34 +59,34 @@ end
 # 
 describe 'String#ellipsis' do
   it "shoud not append ellipsis when String's length is less then 2" do
-  	'l'.ellipsis.should eq 'l'
+    'l'.ellipsis.should eq 'l'
   end
 
   it "shoud not have the max_length value greater than text's length" do
-  	'foo'.ellipsis(10).should eq 'foo'
+    'foo'.ellipsis(10).should eq 'foo'
   end
 
   it "should append ellipsis in the text's half length (default behaviour)" do
-  	'lorem ipsum!'.ellipsis.should eq 'lorem...'
+    'lorem ipsum!'.ellipsis.should eq 'lorem...'
   end
 
   it "should append ellipsis in the text's rounded half length when the number of characters is odd (default behaviour)" do
-  	'lorem ipsum'.ellipsis.should eq 'lorem...'
+    'lorem ipsum'.ellipsis.should eq 'lorem...'
   end
 
   it "should append the HTML encoded ellipsis in the text" do
-  	'lorem ipsum'.ellipsis(5, html_encoded: true).should eq 'lorem&hellip;'
+    'lorem ipsum'.ellipsis(5, html_encoded: true).should eq 'lorem&hellip;'
   end
 
   it "should not append the HTML encoded ellipsis in the text when it's not required" do
-  	'lorem ipsum'.ellipsis(5, html_encoded: false).should_not eq 'lorem&hellip;'
+    'lorem ipsum'.ellipsis(5, html_encoded: false).should_not eq 'lorem&hellip;'
   end
 
   it 'should append ellipsis after a word' do
-  	'lorem ipsum dolor'.ellipsis(14, after_word: true).should eq 'lorem ipsum...'
+    'lorem ipsum dolor'.ellipsis(14, after_word: true).should eq 'lorem ipsum...'
   end
 
   it 'should append the HTML encoded ellipsis after a word' do
-  	'lorem ipsum dolor'.ellipsis(13, html_encoded: true, after_word: true).should eq 'lorem ipsum&hellip;'
+    'lorem ipsum dolor'.ellipsis(13, html_encoded: true, after_word: true).should eq 'lorem ipsum&hellip;'
   end
 end
